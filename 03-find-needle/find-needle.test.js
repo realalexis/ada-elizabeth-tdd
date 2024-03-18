5388452c62797ddfe8f254393a685cfb71a11173
Thomas Sanlis <Thomas.sanlis@gmail.com>
2024-03-18 11:33:17 +0100

    Add 03

---
+++ 03-find-needle/find-needle.test.js
@@ -0,0 +1,17 @@
+const findTheNeedle = require("./find-needle");
+
+test("Find the needle", function () {
+  const words = ["house", "train", "slide", "needle", "book"];
+  const expected = 3;
+
+  const output = findTheNeedle(words, "needle");
+  expect(output).toEqual(expected);
+});
+
+test("Find the plant", function () {
+  const words = ["plant", "shelf", "arrow", "bird"];
+  const expected = 0;
+
+  const output = findTheNeedle(words, "plant");
+  expect(output).toEqual(expected);
+});
