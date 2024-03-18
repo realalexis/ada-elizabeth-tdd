11487f95954b53d22ec8bf3101798b8b5748ea35
Thomas Sanlis <Thomas.sanlis@gmail.com>
2024-03-18 12:21:26 +0100

    Add 05

---
+++ 05-get-average/get-average.test.js
@@ -0,0 +1,10 @@
+const average = require("./get-average");
+
+test("Average", function () {
+  const numbers = [4, "-", 8, 11, "hello", "57", 0, 2];
+  const expected = 5;
+
+  const output = average(numbers);
+
+  expect(output).toEqual(expected);
+});
